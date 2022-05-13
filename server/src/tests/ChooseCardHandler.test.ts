51bc41590b565a941dbca357f24f2727182a8c80
ufr <ufr@dotsource.de>
2022-05-14 01:31:41 +0200

    UFR test and uuid

--- server/src/tests/ChooseCardHandler.test.ts
+++ server/src/tests/ChooseCardHandler.test.ts
@@ -1,21 +1,21 @@
 import { Fibonacci } from '../objects/Fibonacci';
 import { ChooseCardHandler } from '../handler/ChooseCardHandler';
 import { GameState } from '../classes/GameState';
+import { Message } from '../classes/Message';
 
 describe('ChooseCardHandler', () => {
-    const chooseCardMessage: JSON = JSON.parse(`{
-        'method': 'chooseCard',
-        'params': {
+    const chooseCardMessage = new Message();
+    chooseCardMessage.setMethod('chooseCard');
+    chooseCardMessage.setParams({
             'gameId': '9ef90a72-c735-475a-92c9-0e1cabe63926',
             'playerId': 'c315da3e-91f9-4713-9244-7d4a4deb60d1',
             'cardValue': Fibonacci.D,
-        }
-    }`);
+        });
 
     it('ChooseCardHandler', async () => {
         const handler = new ChooseCardHandler();
         const gameState = new GameState();
-        expect(handler.handleMessage(chooseCardMessage, gameState, "")).toMatch(JSON.stringify(chooseCardMessage));
+        expect(handler.handleMessage(chooseCardMessage, gameState, "")).toMatch('');
     });
 });
 
