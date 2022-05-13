b5d48b871c2e45db7f75cc5e681979ee14a1cbe2
ufr <ufr@dotsource.de>
2022-05-13 21:42:28 +0200

    UFR files deletion

--- server/src/handler/RevealCardHandler.ts
+++
@@ -1,8 +0,0 @@
-import { MessageHandler } from "./MessageHandler";
-
-export class RevealCardsHandler implements MessageHandler {
-    handleMessage(message: JSON): string {
-        return JSON.stringify(message);
-    }
-
-}
