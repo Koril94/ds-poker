590d30b9482e6974f0f2f25fd8a6a957dc580a22
Matthias Francke <m.francke@dotsource.de>
2022-05-13 20:31:20 +0200

    first draft of message handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,19 @@ import { WebSocketServer } from "ws";
 import * as ws from "ws";
 import * as http from "http";
 import * as path from "path";
+import uuid from "uuid";
+import { CreateGameHandler } from "./handler/CreateGameHandler";
+import { ChooseCardHandler } from "./handler/ChooseCardHandler";
+import { LeaveGameHandler } from "./handler/LeaveGameHandler";
+import { NewRoundHandler } from "./handler/NewRoundHandler";
+import { ParticipateHandler } from "./handler/ParticipeHandler";
+import { MessageHandler } from "./handler/MessageHandler";
+import { RevealCardHandler } from "./handler/RevealCardHandler";
 
+let playersMap: Map<string, Object>;
 const app = express();
 const __dirname: string = process.env.PWD || "";
-console.log(process.cwd());
-// ... other server.use middleware
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get('/api/test', (req, res) => {
     console.log('Hello World')
 });
@@ -20,15 +27,37 @@ app.get("*", (req, res) => {
 });
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT)
+const PORT = process.env.PORT || 5000;
+server.listen(PORT);
 
 const wss = new WebSocketServer({server});
 
+
 wss.on('connection', (ws) => {
+  const id = uuid.v4();
+  
+  
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', (data) => {
+     try{
+        let handler: MessageHandler; 
+        const dataJson = JSON.parse(data.toString());
+        switch(dataJson["message"]){
+          case "createGame":  handler = new CreateGameHandler(); return handler.handleMessage(dataJson).toString();
+          case "chooseCard":  handler = new ChooseCardHandler(); return handler.handleMessage(dataJson).toString();
+          case "leaveGame":  handler = new LeaveGameHandler(); return handler.handleMessage(dataJson).toString();
+          case "newRound":  handler = new NewRoundHandler(); return handler.handleMessage(dataJson).toString();
+          case "participate":  handler = new ParticipateHandler(); return handler.handleMessage(dataJson).toString();
+          case "revealCart":  handler = new RevealCardHandler(); return handler.handleMessage(dataJson).toString();
+          default: console.log("no matching message found in: %s", JSON.stringify(dataJson));
+        }
+      } catch(e){
+        console.log("something went wrong trying to process the message: %s", e); 
+      };
+     
+
+  });
 });
 
 setInterval(() => {
